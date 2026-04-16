d015f399061feb65d2c1d9af628562b4219f61f4
Tiago <tiagoeclt@gmail.com>
2026-04-16 03:07:24 +0200

    1

---
+++ economy_web/static/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "finances1st-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
